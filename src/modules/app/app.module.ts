d06c152c879d8e56922391fb787b748c6a78ee7a
vinicius <oviniciusfernandes899@gmail.com>
2023-04-17 02:54:27 -0300

    commit ...

--- src/modules/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PersonaModule } from '../persona/persona.module';
-
-@Module({
-  imports: [PersonaModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- src/modules/persona/args/create/create-persona-args.ts
+++
@@ -1,7 +0,0 @@
-export interface CreatePersonaArgs {
-  name: string;
-  description: string;
-  imageURL: string;
-  program: string;
-  author: string;
-}
